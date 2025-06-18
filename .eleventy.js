63557d37a4550069e0c6a2315e0bdceb371104f5
Jeremy Jones <minttoothpick@protonmail.com>
2025-06-18 11:07:30 -0400

    Add blog posts collection

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,27 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = (eleventyConfig) => {
 
+  /**
+   * Collections
+   */
+
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', (collection) => {
+    return [...collection.getFilteredByGlob('./src/blog/*.md')].reverse();
+  });
+
+  /**
+   * Plugins
+   */
+
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  /**
+   * Misc.
+   */
 
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/images');
-
 
   return {
     markdownTemplateEngine: 'njk',
