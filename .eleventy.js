c7ffdf1cb5f4f515db159f061b43fd98c3e9b3fa
Jeremy Jones <minttoothpick@protonmail.com>
2025-06-18 10:21:11 -0400

    Fleshing out layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/images');
 
 
   return {
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist',
     },
   };
 
--- src/index.md
+++ src/index.md
@@ -1,1 +1,5 @@
-# Hi
+---
+title: 'Me yay'
+layout: layouts/home.njk
+---
+Hi, I’m Jeremy. I make things on the web. I'm interested in CSS, accessibility, simplicity, and sharing what I learn.
